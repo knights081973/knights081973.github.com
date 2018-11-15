8b810ea47565c80ad465e0bf2ea57325e602de7d
knights081973 <knights.081973@outlook.com>
2018-11-15 09:27:25 +0800

    Comp
    COmp

--- js/index.js
+++ js/index.js
@@ -1,57 +1,73 @@
 (() => {
 
-	const mobileWidth = 600;
+/* Navigation bar appares when reaches 800 lines in scroll-down - Satart - NOTE: also in CSS changed	*/ 
+	const  mobileWidth = 680;		/*	Using Divce with 600 lines like mobile 	*/
 
 	const addMenuBackground = () => {
-		const boddyOffset = document.body.scrollTop || document.documentElement.scrollTop;
+		const pageWidth = window.innerWidth;	/*	For mobile devices with 600 lines width 	*/
+		const boddyOffset = document.body.scrollTop || document.documentElement.scrollTop;	/*	OR if doesnt work in other browser 	*/
 		const navigation = document.querySelector("header nav");
 
-		if(pageWidth = mobileWidth) {
-				boddyOffset > 700 ? navigation.classList.add("ep-nav-fixed") : navigation.classList.remove("ep-nav-fixed");
+		if(pageWidth > mobileWidth) {
+			boddyOffset > 500 ? navigation.classList.add("ep-nav-fixed") : navigation.classList.remove("ep-nav-fixed");			
 		}
 	}
-// ------------------------------------------------------------------------------------------//
-/*	Creating Humborger Menu    */
+
+/*	Creating Humborger Menu for Mobile device - Start - 	*/
 	const reorderResponsiveMenu = () => {
 		const pageWidth = window.innerWidth;
 		const navContainer = document.querySelector("header nav .ep-container");
 		const navigation = document.querySelector("header nav .ep-navigation");
 		const mobileNavigation = document.querySelector("body > .ep-navigation");
 
+/*	Humboerger - Insert Navigation list to body element - Start - 	*/
 		if (pageWidth <= mobileWidth && navigation) {
-		    document.body.insertAdjacentElement("afterbegin", navigation);
+			document.body.insertAdjacentElement("afterbegin", navigation); 			
 		} else if (pageWidth > mobileWidth && mobileNavigation) {
 			navContainer.insertAdjacentElement("beforeend", mobileNavigation);
 		}
+/*	Humboerger - Insert Navigation list to body element - end - 	*/
 	}
 
-// ------------------------------------------------------------------------------------------//
+/*	Creating Humborger Menu for Mobile device - end - 	*/
 
-	const onNavItemClick = () => { 
-		const navItemList = document.querySelectorAll(".ep-section-link")
+/*	Humborger Menu Click Toggle -Start- 	*/
+	const mobileMenuToggle = () => {
+		const menuToggle = document.querySelector(".ep-nav-toggle");
+
+		menuToggle.addEventListener("click", () => {
+			const mobileNavigation = document.querySelector("body > .ep-navigation");
+
+			mobileNavigation.classList.toggle("ep-navigation-opened")
+
+		})
+
+	}
+
+/*	Humborger Menu Click Toggle -end- 	*/	
+
+
+	const onNavItemClick = () => {
+		const navItemList = document.querySelectorAll(".ep-section-link");
 		const navItems = [...navItemList];
 
 		navItems.forEach(item => {
 			item.addEventListener("click", event => {
-				
 				event.preventDefault();
 
 				const sectionId = event.target.getAttribute("href") || event.target.dataset.href;
 
-	/*			console.log(sectionId);*/
+/*				console.log(sectionId);	/* /*	Observe the Console Log 	*/
 
 				scrollToSection(sectionId);
-
 			})
-
 		})
 	}
 
 	const scrollToSection = sectionId => {
 		let sectionPosition, sectionOffset;
 		const navigationHeight = document.querySelector("header nav").offsetHeight;
-
-		const pageWidth = window.innerWidth;
+		const pageWidth = window.innerWidth
 
 		if(sectionId !== "#") {
 			sectionOffset = document.querySelector(sectionId).offsetTop;
@@ -61,33 +77,33 @@
 			sectionPosition = 0;
 		}
 
-
 		window.scrollTo({
 			'behavior': 'smooth',
 			'left': 0,
 			'top': sectionPosition
 		})
-
 	}
 
+/*		For Left and Right Arrow - Start	*/
 	const onTestimonialChange = () => {
 		let firstChild, lastChild;
-		const preArrow = document.querySelector("#ep-testimonials-prev");
+		const prevArrow = document.querySelector("#ep-testimonials-prev");
 		const nextArrow = document.querySelector("#ep-testimonials-next");
 		const testimonials = document.querySelector(".ep-testimonials ul");
 
 		document.addEventListener("click", () => {
-			if(event.target === preArrow) {
+			if(event.target === prevArrow) {
 				lastChild = testimonials.lastElementChild;
 				testimonials.insertAdjacentElement("afterbegin", lastChild);
 			} else if (event.target === nextArrow) {
 				firstChild = testimonials.firstElementChild;
-				testimonials.insertAdjacentElement("beforeend", firstChild); 
-/*	NOTE: Other options "afterbegin" "afterend" "beforebegin" "beforeend"    */
+				testimonials.insertAdjacentElement("beforeend", firstChild);
 			}
 		})
 	}
+/*		For Left and Right Arrow - end	*/	
 
+/*		Gallery image pop-up on click - start - 	*/ 	
 	const onGalleryImageClick = () => {
 		const galleryImageList = document.querySelectorAll("#ep-gallery li");
 		const galleryImages = [...galleryImageList];
@@ -96,19 +112,19 @@
 			image.addEventListener("click", event => {
 				galleryImageOpen(event.target);
 			})
-		}) 
-	}
+		})
+	}	
 
 	const galleryImageOpen = image => {
 		const imageSrc = image.getAttribute("src");
-/*	NOTE: "`" is not "'" be careful...    */
-		const openedImage = `<div class='ep-backdrop'><img src='${imageSrc}' alt='' />
-							 <span class="ep-backdrop-close">X</span></div>`;
+		const openedImage = `<div class='ep-backdrop'><img src='${imageSrc}' alt=''>
+							<span class="ep-backdrop-close">X</span></div>`;
 
 		document.body.insertAdjacentHTML("beforeend", openedImage);
-		galleryImageClose();
-	}
 
+		galleryImageClose(); /*	Image must close under this function */
+	}
+/*		Closing Image - Start - 	*/
 	const galleryImageClose = () => {
 		const closeButton = document.querySelector(".ep-backdrop-close");
 
@@ -117,18 +133,27 @@
 			backdrop.remove();
 		})
 	}
+/*		Closing Image - end- 	*/
+
+
+/*		Gallery image pop-up on click - ends - 	*/ 		
 
 	window.addEventListener("scroll", () => {
 		addMenuBackground();
 	})
 
+/*		For Humboerger Menu 	*/
 	window.addEventListener("resize", () => {
 		reorderResponsiveMenu();
 	})
+/*		For Humboerger Menu 	*/
 
 	reorderResponsiveMenu();
+	mobileMenuToggle();
 	onNavItemClick();
 	onTestimonialChange();
 	onGalleryImageClick();
 
-})(); 
+
+
+})();
